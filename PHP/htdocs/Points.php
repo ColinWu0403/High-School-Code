e1e2c2e73ff0217ce7ddc405e4c3a32bd6fb1f6e
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-02-02 12:00:50 -0500

    Create Points.php

---
+++ PHP/htdocs/Points.php
@@ -0,0 +1,11 @@
+<?php
+//This program was written by C Wu on 8/31/2021
+
+$point_2 = readline('Enter the amount of 2 pointers scored: ');
+$point_3 = readline('Enter the amount of 3 pointers scored: ');
+
+$point_2_total = $point_2*2;
+$point_3_total = $point_3*3;
+
+echo $point_2_total + $point_3_total;
+?>
